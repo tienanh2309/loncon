fddb2b4bb739127973cf9d65a2b07a9debe579b4
tienanh2309 <96480845+tienanh2309@users.noreply.github.com>
2021-12-21 22:09:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const CONFIG = {
     introDesc: `Alo 1234 ,90 Hà Nam xin chào nhá `,
     btnIntro: 'Trắc nghiệm 1 câu mà đánh sai,thì biết chọn ai giữa 7 tỉ người.',
     title: 'T.anh có đẹp trai ko' 
+    Desc:'???"
     btnYes: 'gud ',
     btnNo: 'sai r lợn con ngốc nghếch ạ ',
     question:'em có bị hâm ko ',
